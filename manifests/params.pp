c2c5e0733504e37803eb419bbeee2962a92bb6c5
SillentTroll <guzun.alex.c@gmail.com>
2014-12-01 23:42:57 +0000

    updated the agent version
    Updated the agent to 2.8.0 version

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,6 @@
 class mongodbmms::params {
   $config_file = '/etc/mongodb-mms/monitoring-agent.config'
-  $deb_filename = 'mongodb-mms-monitoring-agent_2.1.0.35-1_amd64.deb'
+  $deb_filename = 'mongodb-mms-monitoring-agent_2.8.0.143-1_amd64.deb'
   $download_url = 'https://mms.mongodb.com/download/agent/monitoring'
   $mms_server = 'https://mms.mongodb.com'
   $authUsername=''
