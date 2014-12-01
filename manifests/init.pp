6c2cbdbb200dde002b33d4866393ce7d1c1d2dc6
Danny Arnold <danny@kietsy.com>
2014-12-01 16:03:06 +0100

    :lipstick: fix lint error
    - align =>
    - use single quotes where possible
    - use double quotes where necessary
    - don't quote booleans

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,32 +8,32 @@ class mongodbmms (
 ) inherits mongodbmms::params {
 
   exec { 'download-mms':
-    command => "wget ${download_url}/${deb_filename}",
-    path    => ['/bin', '/usr/bin'],
-    cwd    => '/tmp',
+    command   => "wget ${download_url}/${deb_filename}",
+    path      => ['/bin', '/usr/bin'],
+    cwd       => '/tmp',
     logoutput => on_failure,
-    creates => '/tmp/${deb_filename}'
+    creates   => "/tmp/${deb_filename}",
   }
 
   exec { 'install-mms':
-    command => "sudo dpkg -i ${deb_filename}",
-    path    => ['/bin', '/usr/bin'],
-    cwd    => '/tmp',
+    command   => "sudo dpkg -i ${deb_filename}",
+    path      => ['/bin', '/usr/bin'],
+    cwd       => '/tmp',
     logoutput => on_failure,
-    require => [Exec['download-mms']]
+    require   => [Exec['download-mms']],
   }
 
   file { '/etc/mongodb-mms/monitoring-agent.config':
-    content=>template('mongodbmms/monitoring-agent.config.erb'),
-    require => [Exec['install-mms']]
+    content => template('mongodbmms/monitoring-agent.config.erb'),
+    require => [Exec['install-mms']],
   }
 
-  service { "mongodb-mms-monitoring-agent":
+  service { 'mongodb-mms-monitoring-agent':
     ensure     => 'running',
     provider   => 'upstart',
-    hasrestart => 'true',
-    hasstatus  => 'true',
-    require => File['/etc/mongodb-mms/monitoring-agent.config'],
-    subscribe  => File['/etc/mongodb-mms/monitoring-agent.config']
+    hasrestart => true,
+    hasstatus  => true,
+    require    => File['/etc/mongodb-mms/monitoring-agent.config'],
+    subscribe  => File['/etc/mongodb-mms/monitoring-agent.config'],
   }
 }
